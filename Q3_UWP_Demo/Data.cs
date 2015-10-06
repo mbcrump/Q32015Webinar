d0c9ba36c6f495997aef77e4067c129593b2a355
mbcrump <mbcrump29@gmail.com>
2015-10-06 13:52:20 -0500

    UWP Demo

---
+++ Q3_UWP_Demo/Data.cs
@@ -0,0 +1,8 @@
+﻿namespace Q3_UWP_Demo
+{
+    public class Data
+    {
+        public string CompanyName { get; set; }
+        public double Value { get; set; }
+    }
+}
---
+++ Q3_UWP_Demo/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Q3_UWP_Demo
+{
+
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            Loaded += MainPage_Loaded;
+        }
+
+        private void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            List<Data> data = new List<Data>();
+            data.Add(new Data() { CompanyName = "Microsoft", Value = 9 });
+            data.Add(new Data() { CompanyName = "Google", Value = 5 });
+            data.Add(new Data() { CompanyName = "Apple", Value = 7 });
+            data.Add(new Data() { CompanyName = "SalesForce", Value = 3 });
+            data.Add(new Data() { CompanyName = "SAP", Value = 15 });
+
+            this.chart.DataContext = data;
+        }
+    }
+}
